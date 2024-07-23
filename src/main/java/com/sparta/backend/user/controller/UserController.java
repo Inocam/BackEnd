4f770497317806b50e5aa11a814b0e2678f36d04
JISOO\boy74 <boy7442@gmail.com>
2024-07-24 04:40:16 +0900

    Jwt를 이용한 로그인 구현 + Kakao 로그인 access_token 가져오기 및 카카오 사용자 정보 가져오기 구현

---
+++ src/main/java/com/sparta/backend/user/controller/UserController.java
@@ -0,0 +1,80 @@
+package com.sparta.backend.user.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sparta.backend.user.security.JwtUtil;
+import com.sparta.backend.user.security.UserDetailsImpl;
+import com.sparta.backend.user.service.KakaoService;
+import com.sparta.backend.user.service.UserService;
+import com.sparta.backend.user.dto.SignupRequestDto;
+import com.sparta.backend.user.dto.UserInfoDto;
+import com.sparta.backend.user.model.UserRoleEnum;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+    private final KakaoService kakaoService;
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
+        userService.signup(requestDto);
+
+        return "redirect:/api/user/login-page";
+    }
+
+    // 회원 관련 정보 받기
+    @GetMapping("/user-info")
+    @ResponseBody
+    public UserInfoDto getUserInfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        String username = userDetails.getUser().getUsername();
+        UserRoleEnum role = userDetails.getUser().getRole();
+        boolean isAdmin = (role == UserRoleEnum.ADMIN);
+
+        return new UserInfoDto(username, isAdmin);
+    }
+
+    @GetMapping("/user/kakao/callback")
+    public String kakaoCallback(@RequestParam String code, HttpServletResponse response, @AuthenticationPrincipal UserDetailsImpl userDetails) throws JsonProcessingException {
+        String token = kakaoService.kakaoLogin(code);
+
+        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, token);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return "redirect:/";
+    }
+}
