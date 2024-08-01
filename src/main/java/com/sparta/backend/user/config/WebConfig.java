41428aa7247f916b42c5e26a9db4210249b7be0a
JISOO\boy74 <boy7442@gmail.com>
2024-08-01 10:34:34 +0900

    CORS 모든 출처 허용

--- src/main/java/com/sparta/backend/user/config/WebConfig.java
+++ src/main/java/com/sparta/backend/user/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 엔드포인트에 대해 CORS를 허용
-                .allowedOrigins("http://localhost:5173")  // 허용할 출처
-                .allowedOrigins("https://foot.o-r.kr")
+                .allowedOriginPatterns("*")  // 모든 출처 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메소드
                 .allowedHeaders("*");  // 허용할 HTTP 헤더
     }
