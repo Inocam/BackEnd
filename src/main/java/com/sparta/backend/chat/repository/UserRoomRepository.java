1bcd7cd2df5da72a9768b8be53022ddf0976e3fc
won <januse159@naver.com>
2024-08-02 09:34:30 +0900

    사용자가 속한 채팅방 목록 기능 구현

--- src/main/java/com/sparta/backend/chat/repository/UserRoomRepository.java
+++ src/main/java/com/sparta/backend/chat/repository/UserRoomRepository.java
@@ -4,6 +4,11 @@ import com.sparta.backend.chat.entity.ChatRoom;
 import com.sparta.backend.chat.entity.UserRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRoomRepository extends JpaRepository<UserRoom, Long> {
     void deleteByChatRoom(ChatRoom chatRoom);
+
+    List<UserRoom> findAllByUserId(Long userId);
 }
