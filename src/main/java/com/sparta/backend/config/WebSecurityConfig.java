138fb2b29d22b5c2d1e44e8b9641d1f047f747ab
JISOO\boy74 <boy7442@gmail.com>
2024-08-10 12:42:40 +0900

    swagger 관련 v3 하위 폴더 접근 허가

--- src/main/java/com/sparta/backend/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/backend/config/WebSecurityConfig.java
@@ -71,6 +71,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .requestMatchers("/swagger-ui.html").permitAll()
                         .requestMatchers("/swagger-ui/*").permitAll()
+                        .requestMatchers("/v3/*").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
