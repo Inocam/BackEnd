4e2ced2edd5a3fb963ef28ce3d30ea493ff104b5
JISOO\boy74 <boy7442@gmail.com>
2024-08-14 00:01:01 +0900

    메인페이지 요청 허가

--- src/main/java/com/sparta/backend/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/backend/config/WebSecurityConfig.java
@@ -70,7 +70,7 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                        //.requestMatchers("/").permitAll() // 메인 페이지 요청 허가
+                        .requestMatchers("/index.html").permitAll() // 메인 페이지 요청 허가
                         .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .requestMatchers("/swagger-ui.html").permitAll()
                         .requestMatchers("/swagger-ui/*").permitAll()
